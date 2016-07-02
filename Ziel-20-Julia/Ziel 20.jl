f68585e9d273eef688d75214af7392c1d6e1cc2d
dystr <dystr@protonmail.com>
2016-07-02 15:12:12 +0200

    Create Ziel 20.jl
    Game with the goal to reach the score of 20 first.
    Players: 2
    
    The players are given a random value(1:6) and the current score and can decide whether to add the value to their own score or to subtract it from the opponents.
    
    Default player one is user input, player 2 is automatic.

---
+++ Ziel-20-Julia/Ziel 20.jl
@@ -0,0 +1,34 @@
+function game(player=1,score=[0,0])
+    if score[1] == 20; println("Player 1 wins!")
+        elseif score[2] == 20; println("Player 2 wins!")
+    else
+        move(player,score)
+    end
+end
+
+function move(player,score)
+    getmove(player)(player,score,rand(1:6))
+end
+
+function getmove(player)
+    if player == 1
+        return move1
+    else
+        return move2
+    end
+end
+
+function move1(score,roll)
+    println(score)
+    println("You'r roll is $roll !")
+    if readline(STDIN)== "+"
+        game(2,[score[1]+=roll,score[2]])
+    else
+        game(2,[score[1]-=roll,score[2]])
+    end
+end
+
+function move2(score,roll)
+    score[2]+=roll
+    game(1,score)
+end
